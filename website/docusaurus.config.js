22fd44669e4d0a095c7a8be22d95df5211ec42d2
Andi Miller <andi@andimiller.net>
2021-11-22 16:29:34 +0000

    put our twitter in the footer

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -80,16 +80,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/meltwatereng',
               },
             ],
           },
